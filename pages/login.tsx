a92f01f1f9dd7229ebf1988a5abf92ed65d93ee8
Cristian Windows Predator <xthecapx@gmail.com>
2021-11-21 18:31:42 -0500

    added error to login screen

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,6 +2,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import firebase from '../firebase/clientApp'
 import mainImage from '../public/images/proposal/proposal9.jpeg'
+import useBreakpoint from '../hooks/useBreakpoint'
+import clsx from 'clsx'
 
 import Image from 'next/image'
 import { useState } from 'react'
@@ -9,19 +11,28 @@ import { useState } from 'react'
 // Custom App to wrap it with context provider
 export default function Login(): JSX.Element {
   const [email, setEmail] = useState('')
+  const [error, setError] = useState('')
+  const breakpoint = useBreakpoint()
   const login = async (email: string) => {
+    const user = email.toLocaleLowerCase()
+
     try {
-      await firebase.auth().signInWithEmailAndPassword(email, email)
+      setError('')
+      await firebase.auth().signInWithEmailAndPassword(user, user)
     } catch (error) {
-      // console.log(error)
+      setError('El email no es valido')
     }
   }
+  const isMobile = breakpoint === 'xs'
 
   return (
     <>
       <div className="w-full h-screen md:-mx-4 login-bg" />
       <div
-        className="absolute w-3/5 bg-white"
+        className={clsx('absolute bg-white', {
+          'w-3/5': !isMobile,
+          'w-full': isMobile,
+        })}
         style={{ transform: 'translate(-50%, -50%)', top: '50%', left: '50%' }}
       >
         <div className="flex justify-center -mt-10">
@@ -36,22 +47,39 @@ export default function Login(): JSX.Element {
             height="80"
           />
         </div>
-        <div className="px-12 py-10">
+        <div
+          className={clsx({
+            'px-12 py-10': !isMobile,
+            'px-4 py-4': isMobile,
+          })}
+        >
           <div className="w-full mb-2">
-            <div className="flex relative ">
+            <div
+              className={clsx('flex relative', {
+                'border border-red-300 rounded': error,
+              })}
+            >
               <span className="rounded-l-md inline-flex items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
                 <FontAwesomeIcon icon={faEnvelope} />
               </span>
               <input
                 type="text"
                 id="email-with-icon"
-                className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                className="rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:border-transparent"
                 name="email"
                 placeholder="Your email"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => {
+                  setError('')
+                  setEmail(e.target.value)
+                }}
               />
             </div>
+            {error && (
+              <div className="mt-2 flex relative">
+                <p className="text-red-500 text-xs italic">{error}</p>
+              </div>
+            )}
           </div>
           <div className="mt-8 flex justify-between">
             <button
