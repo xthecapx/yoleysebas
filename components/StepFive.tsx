432d596486554c997ecb7eea4eca2f00b21deec7
Cristian Windows Predator <xthecapx@gmail.com>
2022-01-15 16:20:22 -0500

    fix: new copy for step five

--- components/StepFive.tsx
+++ components/StepFive.tsx
@@ -1,7 +1,7 @@
 export default function StepFive(): JSX.Element {
   return (
     <p className="mb-4 sec-description">
-      {`Para nosotros lo más importante es que puedas acompañarnos en nuestro gran día. Ahora bien, si deseas apoyarnos, puedes hacerlo el día de la recepción.`}
+      {`El día de la recepción tendremos un espacio donde puedas entregarlo. En todo caso, ten en cuenta que para nosotros lo más importante es que puedas acompañarnos en nuestro gran día.`}
     </p>
   )
 }
